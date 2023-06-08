5f063a73fbdc80ad8adb01a8d1e9dea79265c278
Jeffrey Brite <jeff@techcollective.com>
2023-06-08 13:27:57 -0500

    mroe setup.py

--- setup.py
+++ setup.py
@@ -9,6 +9,7 @@ setup(name='pyautotask',
       author_email='jeff@techcollective.com',
       url='https://github.com/TechCollective/pyautotask',
       packages=['pyautotask'],
+      scripts=[],
       classifiers=[],
       install_requires=['requests'],
       )
