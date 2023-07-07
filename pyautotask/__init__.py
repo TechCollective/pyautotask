1e1f56a14fcf6f7fdee0c875d9b389f0a6dfa153
Jeffrey Brite <jeff@techcollective.com>
2023-07-07 15:49:13 -0500

    updaing __init__

--- pyautotask/__init__.py
+++ pyautotask/__init__.py
@@ -1,9 +1,10 @@
 """
 Python __init__ to interact with an Autotask site
 """
+from __future__ import absolute_import
 import urllib3
-#from pyautotask.models.ticket_entity_info.py import TicketEntityInfo
-
+from pyautotask.models.ticket_entity_info.py import TicketEntityInfo
+import pyautotask.atsite
 
 def http_debug_log_stderr():
     """Dump requests urllib3 debug messages to stderr"""
